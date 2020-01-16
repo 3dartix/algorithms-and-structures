27a816ecf862520be96d81d452adf8793e45299c
Artem <info@3dartix.ru>
2020-01-16 15:52:30 +0200

    Возведение в степень и задача с рюкзаком с помощью рекурсии

---
+++ src/main/java/lesson5/Item.java
@@ -0,0 +1,13 @@
+package lesson5;
+
+public class Item {
+    String name;
+    float weight;
+    float price;
+
+    public Item (String name, float weight, float price){
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+}
